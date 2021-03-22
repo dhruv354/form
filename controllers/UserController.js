6d27a4b974cf1c9d5957d825904181d03111e1ac
dhruv singhal <dhruv.singhal2612@gmail.com>
2021-03-22 13:05:22 +0530

    updated userController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,9 @@ const User = require('../models/User')
 const nodemailer = require('nodemailer')
 
 module.exports.User = (req, res) => {
+    //check if password and confirm password entered are same or not
     if(req.body.password != req.body.confirm_password){
+        res.send('<script>alert("password and confirm password dont match")</script>')
         res.redirect('back')
     }
     
@@ -16,22 +18,25 @@ module.exports.User = (req, res) => {
                 if(err){console.log('error');}
                 console.log('User created');
             })
+            //create a create transport object with your credentials and which email service you want to use
+            //use your gmail email and password here 
             let mailTransporter = nodemailer.createTransport({ 
                 service: 'gmail', 
                 host: 'smtp.gmail.com',
                 auth: { 
-                    user: 'dhruv.singhal2612@gmail.com',
-                    pass: 'Somya@2612'
+                    user: 'dhruv.singhal2612@gmail.com',//your email here i have used my email
+                    pass: your_password
                 } 
             }); 
               
+            //details about whom to send with email subject and message
             let mailDetails = { 
-                from: '"dhruv.singhal2612" <dhruv.singhal2612@gmail.com>', 
+                from: '"dhruv.singhal2612" <dhruv.singhal2612@gmail.com>', //your email
                 to: req.body.email, 
                 subject: 'from dhruv singhal as a project', 
                 text: 'your data is successfully stored on our database'
             }; 
-              
+            //send email with a callback regarding success of the event
             mailTransporter.sendMail(mailDetails, function(err, data) { 
                 if(err) { 
                     console.log(err); 
@@ -41,9 +46,10 @@ module.exports.User = (req, res) => {
             });
             return res.send('<script>alert("User created")</script>');
         }
+        // if user has already sent an email just tell him that he has been already added in the database
         else{
             console.log('User already present in the database');
-            res.send('<script>alert("your have already got mail from our side")</script>')
+            return res.send('<script>alert("your have already got mail from our side")</script>')
         }
     })
 }
